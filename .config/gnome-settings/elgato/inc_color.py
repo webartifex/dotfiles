e98a39091ecf84d827580b171219139112ce1fc2
Alexander Hess <alexander@webartifex.biz>
2023-05-08 13:47:22 +0200

    Adjust keyboard shortcuts for the Elgato Key Lights
    - add keyboard shortcuts to increase/decrease the color temperature
    - simplify (i.e., unify) all the shortcuts to only come in one variant

---
+++ .config/gnome-settings/elgato/inc_color.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import leglight
+
+LEFT_KEYLIGHT_IP = "192.168.190.62"
+RIGHT_KEYLIGHT_IP = "192.168.190.63"
+
+left = leglight.LegLight(LEFT_KEYLIGHT_IP, port=9123)
+right = leglight.LegLight(RIGHT_KEYLIGHT_IP, port=9123)
+
+left.incColor(200)
+right.incColor(200)
