8ebea885385d808168a78407d389e24e16bc9d8b
Alexander Hess <alexander@webartifex.biz>
2024-06-06 19:02:07 +0200

    Fix logic error

--- .config/shell/logout.sh
+++ .config/shell/logout.sh
@@ -2,5 +2,9 @@
 
 # Clear the screen to increase privacy
 if [ "$SHLVL" = 1 ]; then
-    [ -x /usr/bin/clear ] && /usr/bin/clear || [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+    if [ -x /usr/bin/clear ]; then
+        /usr/bin/clear
+    elif [ -x /usr/bin/clear_console ]; then
+        /usr/bin/clear_console -q
+    fi
 fi
