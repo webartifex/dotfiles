9ae6cebc17ef06e8af47df1976f47eb309b57335
Alexander Hess <alexander@webartifex.biz>
2024-07-02 16:20:07 +0200

    Do not clear the screen upon logout

--- .config/shell/logout.sh
+++ .config/shell/logout.sh
@@ -1,18 +1,1 @@
 # This file is sourced by a login shell upon logout
-
-# Clear the screen (if desired) to increase privacy
-if [ "$SHLVL" = 1 ]; then
-    if [ -x /usr/bin/clear ]; then
-        read -p "Clear screen? " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            /usr/bin/clear
-        fi
-    elif [ -x /usr/bin/clear_console ]; then
-        read -p "Clear screen? " -n 1 -r
-        echo
-        if [[ $REPLY =~ ^[Yy]$ ]]; then
-            /usr/bin/clear_console -q
-        fi
-    fi
-fi
